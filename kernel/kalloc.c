4531581fd57d2d17bd7a67004253f8fdb9df63cf
1234 <2154343@tongji.edu.cn>
2024-08-29 14:09:44 +0800

    first

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -22,10 +22,26 @@ struct {
   struct spinlock lock;
   struct run *freelist;
 } kmem;
+struct ref_stru {
+  struct spinlock lock;
+  int cnt[PHYSTOP / PGSIZE];  // 引用计数 最大物理地址除以页面大小，为每一个物理地址建一个映射
+} ref;
 
+int krefcnt(void* pa) {  // 获取内存的引用计数
+  return ref.cnt[(uint64)pa / PGSIZE];
+}
+int kaddrefcnt(void* pa) {
+  if(((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP)
+    return -1;
+  acquire(&ref.lock);
+  ++ref.cnt[(uint64)pa / PGSIZE];
+  release(&ref.lock);
+  return 0;
+}
 void
 kinit()
 {
+  initlock(&ref.lock, "ref"); 
   initlock(&kmem.lock, "kmem");
   freerange(end, (void*)PHYSTOP);
 }
@@ -35,8 +51,11 @@ freerange(void *pa_start, void *pa_end)
 {
   char *p;
   p = (char*)PGROUNDUP((uint64)pa_start);
-  for(; p + PGSIZE <= (char*)pa_end; p += PGSIZE)
+  for(; p + PGSIZE <= (char*)pa_end; p += PGSIZE){
+      // 在kfree中将会对cnt[]减1，这里要先设为1，否则就会减成负数
+    ref.cnt[(uint64)p / PGSIZE] = 1;
     kfree(p);
+  }
 }
 
 // Free the page of physical memory pointed at by v,
@@ -50,16 +69,24 @@ kfree(void *pa)
 
   if(((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP)
     panic("kfree");
+  // 只有当引用计数为0了才回收空间
+  // 否则只是将引用计数减1
+  acquire(&ref.lock);
+  if(--ref.cnt[(uint64)pa / PGSIZE] == 0) {
+      release(&ref.lock);
 
   // Fill with junk to catch dangling refs.
-  memset(pa, 1, PGSIZE);
+      memset(pa, 1, PGSIZE);
 
-  r = (struct run*)pa;
+      r = (struct run*)pa;
 
-  acquire(&kmem.lock);
-  r->next = kmem.freelist;
-  kmem.freelist = r;
-  release(&kmem.lock);
+      acquire(&kmem.lock);
+      r->next = kmem.freelist;
+      kmem.freelist = r;
+      release(&kmem.lock);
+  }else{
+      release(&ref.lock);
+  }
 }
 
 // Allocate one 4096-byte page of physical memory.
@@ -72,11 +99,70 @@ kalloc(void)
 
   acquire(&kmem.lock);
   r = kmem.freelist;
-  if(r)
+  if(r){
     kmem.freelist = r->next;
+     acquire(&ref.lock);
+    ref.cnt[(uint64)r / PGSIZE] = 1;  // 将引用计数初始化为1
+    release(&ref.lock);}
+    
   release(&kmem.lock);
 
   if(r)
     memset((char*)r, 5, PGSIZE); // fill with junk
   return (void*)r;
+}
+//  cowpage 判断一个页面是否为COW页面
+int cowpage(pagetable_t pagetable, uint64 va) {
+  if(va >= MAXVA)
+    return -1;
+  pte_t* pte = walk(pagetable, va, 0);
+  if(pte == 0)
+    return -1;
+  if((*pte & PTE_V) == 0)
+    return -1;
+  return (*pte & PTE_F ? 0 : -1);
+}
+
+//cowalloc copy-on-write分配器
+
+void* cowalloc(pagetable_t pagetable, uint64 va) {
+  if(va % PGSIZE != 0)
+    return 0;
+
+  uint64 pa = walkaddr(pagetable, va);  // 获取对应的物理地址
+  if(pa == 0)
+    return 0;
+
+  pte_t* pte = walk(pagetable, va, 0);  // 获取对应的PTE
+
+  if(krefcnt((char*)pa) == 1) {
+    // 只剩一个进程对此物理地址存在引用
+    // 则直接修改对应的PTE即可
+    *pte |= PTE_W;
+    *pte &= ~PTE_F;
+    return (void*)pa;
+  } else {
+    // 多个进程对物理内存存在引用
+    // 需要分配新的页面，并拷贝旧页面的内容
+    char* mem = kalloc();
+    if(mem == 0)
+      return 0;
+
+    // 复制旧页面内容到新页
+    memmove(mem, (char*)pa, PGSIZE);
+
+    // 清除PTE_V，否则在mappagges中会判定为remap
+    *pte &= ~PTE_V;
+
+    // 为新页面添加映射
+    if(mappages(pagetable, va, PGSIZE, (uint64)mem, (PTE_FLAGS(*pte) | PTE_W) & ~PTE_F) != 0) {
+      kfree(mem);
+      *pte |= PTE_V;
+      return 0;
+    }
+
+    // 将原来的物理内存引用计数减1
+    kfree((char*)PGROUNDDOWN(pa));
+    return mem;
+  }
 }
